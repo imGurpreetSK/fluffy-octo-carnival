4c6d22d56499bc318db36df011cbd8b672cac275
Gurpreet Singh <gurpreetsk@hotmail.com>
2025-02-08 00:13:34 -0800

    Introduce :shared:analytics.

---
+++ shared/analytics/src/androidDeviceTest/kotlin/com/gurpreetsk/shared/analytics/CanaryInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package com.gurpreetsk.shared.analytics
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class CanaryInstrumentedTest {
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.gurpreetsk.shared.analytics.test", appContext.packageName)
+    }
+}
---
+++ shared/analytics/src/androidHostTest/kotlin/com/gurpreetsk/shared/analytics/CanaryUnitTest.kt
@@ -0,0 +1,12 @@
+package com.gurpreetsk.shared.analytics
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CanaryUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
