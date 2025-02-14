b5eafb84596ac9ac0679b94b9af80f1d4bd3cb09
Gurpreet Singh <gurpreetsk@hotmail.com>
2025-02-14 13:25:21 -0800

    Configure storage and network modules for iOS app.

--- composeApp/src/iosMain/kotlin/com/gurpreetsk/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/gurpreetsk/MainViewController.kt
@@ -2,12 +2,17 @@ package com.gurpreetsk
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.gurpreetsk.di.initKoin
-import com.gurpreetsk.di.iosAppModule
+import com.gurpreetsk.navigation.HomeScreenNavigation
+import com.gurpreetsk.shared.network.iOSNetworkModule
+import com.gurpreetsk.shared.storage.di.iOSStorageModule
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
         initKoin {
-            modules(iosAppModule)
+            modules(
+                iOSStorageModule,
+                iOSNetworkModule
+            )
         }
     }
 ) {
